fe025c56585054842483948a6b3563b60b90ee3a
zabudskajuliia <zabudska.juliia@gmail.com>
2016-08-29 20:08:28 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Soldier soldier = new Soldier(new Laser(20));
+        Soldier soldier = new Soldier(new Gun(30));
         soldier.run(20.0);
         soldier.shoot(10);
-        soldier.setWeapon(new Gun(30));
+        soldier.setWeapon(new Laser(40));
         soldier.shoot(100);
     }
 }
